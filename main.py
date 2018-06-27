14d2f56ba084b299605d254276c7ea2e3518e51f
truefinch <glushkov.vk@students.dvfu.ru>
2018-06-27 15:22:27 +1000

    .

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
 import json
+
 app = Flask(__name__)
 
 
 # model = Model()
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
 
 
 @app.route("/hook", methods=["POST", "GET"])
